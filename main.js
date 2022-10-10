8459959ec7d478516702ba79f147632f91661935
aziz <erazizeee@gmail.com>
2022-10-10 12:18:35 +0530

    MonishaMam-Task

---
+++ main.js
@@ -0,0 +1,37 @@
+
+// Fibonacci Series
+
+// const number = parseInt(prompt('Enter the number'));
+// let n1 = 0, n2 = 1, nextTerm;
+
+// console.log('Fibonacci Series:');
+
+// for (let i = 1; i <= number; i++) {
+//     console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
+
+
+// Fibonacci Series
+var count = 20;
+var n1 = 0, n2 = 1, nextdigit;
+console.log(count + " of fibonacci series:")
+for (i = 1; i <= count; i++) {
+    console.log(n1);
+    nextdigit = n1 + n2;
+    n1 = n2;
+    n2 = nextdigit;
+
+}
+
+// Email ODD Number
+
+var email = prompt("Enter Your Mail");
+var l = email.length;
+for (i = 0; i < l; i++) {
+    if (email[i] == 1 || email[i] == 3 || email[i] == 5 || email[i] == 7 || email[i] == 9) {
+        console.log(email[i]);
+    }
+}
